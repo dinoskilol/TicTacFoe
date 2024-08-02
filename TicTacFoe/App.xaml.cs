419075763620536da725e67ab2ff2e7adaaf13ad
dinoskilol <dinoyordanov@gmail.com>
2024-08-02 09:35:20 +0200

    1.0.0 release

---
+++ TicTacFoe/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAppSDK;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+
+namespace TicTacFoe
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        // Change window size and center on Windows
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+
+            // Change the window size
+            window.Width = 500; window.Height = 550;
+            window.Title = "TicTacFoe";
+
+            // Center the window
+            var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+            window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
+            window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+            return window;
+        }
+    }
+}
---
+++ TicTacFoe/MauiProgram.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+
+
+namespace TicTacFoe
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })         
+  .ConfigureLifecycleEvents(events =>
+
+   {
+
+
+#if WINDOWS
+
+       events.AddWindows(wndLifeCycleBuilder =>
+
+       {
+
+           wndLifeCycleBuilder.OnWindowCreated(window =>
+
+           {
+
+               IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+               WindowId nativeWindowId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+
+               AppWindow appWindow = AppWindow.GetFromWindowId(nativeWindowId);
+
+               var p = appWindow.Presenter as OverlappedPresenter;
+
+               window.ExtendsContentIntoTitleBar = false;
+               p.SetBorderAndTitleBar(false, false);
+               p.IsResizable = false;
+           });
+
+       });
+
+#endif
+
+   });
+
+            builder.Services.AddMauiBlazorWebView();
+
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+
+            return builder.Build();
+        }
+    }
+}
